7d7ab2ced963634efb591c3b6bb34d25095b9222
Shreekar11 <shreekargade2004@gmail.com>
2024-09-11 16:42:28 +0530

    form and folders fixed

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,14 @@
 'use client'
 import { useState } from 'react';
 import { NextPage } from 'next';
+import { Button } from '@/components/ui/button';
 import Link from 'next/link';
+import Image from 'next/image';
 
+// images
+import phone from "../../../public/assets/auth/phone.png"
+import profile from "../../../public/assets/auth/profile.png"
+import lock from "../../../public/assets/auth/lock.png"
 
 const LoginPage: NextPage = () => {
   const [email, setEmail] = useState<string>('');
@@ -14,41 +20,80 @@ const LoginPage: NextPage = () => {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-black bg-cover bg-center" style={{ backgroundImage: "url('/vecteezy_retro-sci-fi-futuristic-background-1980s-and-1990s-style-3d_7795861-ai1.png')" }}>
-      <div className="p-8 rounded-3xl shadow-lg w-[638px] h-[717px] bg-opacity-65 bg-logincardcolor border-loginbordercolor border-4 flex flex-col items-center">
-        <div className="flex w-[417.18px] items-center mt-16">
-          <img src="/image 16.png" alt="Gameboy Icon" className="h-24 w-16" />
-          <h1 className="font-vt323 text-9xl text-white w-[363.18px] text-center">LOGIN</h1>
+    <div
+      className="flex items-center justify-center min-h-screen bg-black bg-cover bg-center"
+      style={{
+        backgroundImage:
+          "url('/assets/auth/vector-bg.png')",
+      }}
+    >
+      <div className="p-8 rounded-3xl shadow-lg bg-opacity-65 bg-logincardcolor border-loginbordercolor border-4 flex flex-col items-center">
+        <div className="flex text-3xl md:text-5xl items-center">
+          <Image
+            src={phone}
+            alt="Gameboy Icon"
+            height={30}
+            width={30}
+            className="mr-4"
+          />
+          <h1 className="font-vt323 text-white text-center">LOGIN</h1>
         </div>
-        <form onSubmit={handleLogin} className='flex flex-col items-center'>
-          <div className="relative w-[413.56px] h-[58.89px] mt-[62.11px]">
+        <form
+          onSubmit={handleLogin}
+          className="flex flex-col items-center space-y-10 mt-10 w-full"
+        >
+          <div className="relative w-full">
             <input
               type="text"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              className="w-full h-full bg-opacity-[0.92] bg-inputfildcolor rounded-[39px] pl-[69.2px] text-sm text-white placeholder-white border-none"
+              className="w-full p-2 bg-opacity-[0.92] bg-inputfildcolor rounded-[39px] pl-[69.2px] text-sm text-white placeholder-white border-none"
             />
-            <img src="/pfp.png" className="absolute left-0 top-1/2 transform -translate-y-1/2 w-[69.2px] h-[58.89px]" alt="Profile Icon" />
+            <Image
+              src={profile}
+              height={40}
+              width={40}
+              className="absolute left-0 top-1/2 transform -translate-y-1/2"
+              alt="Profile Icon"
+            />
           </div>
-          <div className="relative w-[413.56px] h-[58.89px] mt-[62.11px]">
+          <div className="relative w-full">
             <input
               type="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              className="w-full h-full bg-opacity-[0.92] bg-inputfildcolor rounded-[39px] pl-[69.2px] text-sm text-white placeholder-white border-none"
+              className="w-full p-2 bg-opacity-[0.92] bg-inputfildcolor rounded-[39px] pl-[69.2px] text-sm text-white placeholder-white border-none"
             />
-            <img src="/Rectangle 11.png" className="absolute left-0 top-1/2 transform -translate-y-1/2 w-[69.2px] h-[58.89px]" alt="Profile Icon" />
+            <Image
+              src={lock}
+              height={40}
+              width={40}
+              className="absolute left-0 top-1/2 transform -translate-y-1/2"
+              alt="Profile Icon"
+            />
           </div>
-          <a href="#" className="hover:underline font-vt323 text-xl text-white text-left mt-[23.11px] w-full">Forgot password?</a>
-          <button
+        </form>
+        <Link
+            href="/forgot-password"
+            className="hover:underline font-vt323 text-xl text-white text-left mt-2 w-full"
+          >
+            Forgot password?
+          </Link>
+          <Button
+            variant={"default"}
+            className=" hover:bg-opacity-60 mt-5 px-10 py-8 w-full text-lg font-bold text-white bg-submitbtncolor rounded-full"
             type="submit"
-            className="mt-5 w-[251.14px] h-[61.91px] py-3 text-lg font-bold text-white bg-submitbtncolor rounded-[39px] "
           >
             Login
-          </button>
-        </form>
-        <div className="font-vt323 text-2xl text-center text-white mt-4">
-          Don&apos;t have an account? <Link href="register" className="font-vt323 text-loginsignuplinkcolor hover:underline">Register</Link>
+          </Button>
+        <div className="font-vt323 text-xl text-center text-white mt-4">
+          Don&apos;t have an account?{" "}
+          <Link
+            href="/register"
+            className="font-vt323 text-loginsignuplinkcolor hover:underline"
+          >
+            Register
+          </Link>
         </div>
       </div>
     </div>

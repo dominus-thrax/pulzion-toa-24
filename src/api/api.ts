2da580028d17ed3e12f6dfa5f32ca3d7a6181fa1
Shreekar11 <shreekargade2004@gmail.com>
2024-09-13 00:46:03 +0530

    add to cart pr commit

--- src/api/api.ts
+++ src/api/api.ts
@@ -7,9 +7,7 @@ const api = axios.create({
 
 api.interceptors.request.use(
   (config) => {
-    // const token = localStorage.getItem("token");
-    const token =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiaWF0IjoxNzI1OTUzMTc1fQ._LWlL7w4Y3xpOBhGEjB4zTJxpDfEGJ0TvV4PmSnL9yg";
+    const token = localStorage.getItem("token");
     if (token) {
       config.headers["Authorization"] = `Bearer ${token}`;
     }

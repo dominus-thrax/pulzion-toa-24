d74ae2be113d0cc7b5d7273e7e1210d3079bb642
Shreekar11 <shreekargade2004@gmail.com>
2024-09-12 16:37:09 +0530

    events updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,6 @@
-// src/app/page.tsx
-import EventCard from '@/components/ui/EventCard';
-import Card from '../components/ui/Card';
-
 export default function Home() {
   return (
     <main>
-      
     </main>
   );
 }

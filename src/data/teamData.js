e9dd2216bd120f02c1ed14a1774cb4f9d56d8114
Devashish-Bhirdi <133349700+Devashish-Bhirdi@users.noreply.github.com>
2024-09-12 12:21:58 +0530

    Improved Code

---
+++ src/data/teamData.js
@@ -0,0 +1,275 @@
+export const teamMembers = [
+    {
+      name: 'Devashish Bhirdi',
+      position: 'EMS Frontend Lead',
+      image: '/assets/teams/images.jpeg',
+      linkedin: 'https://www.linkedin.com/in/devashish-bhirdi-53835529a/?trk=opento_nprofile_details'
+    },
+    {
+      name: 'Shreekar Gade',
+      position: 'EMS Frontend Lead',
+      image: '/assets/teams/images.jpeg',
+      linkedin: 'https://www.linkedin.com/in/shreekar-gade-2a9937257/'
+    },
+    {
+      name: 'Omkar Gade',
+      position: 'EMS Backend Lead',
+      image: '/assets/teams/images.jpeg',
+      linkedin: ''
+    },
+    {
+      name: 'Balaji Shaw',
+      position: 'EMS Backend Lead',
+      image: '/assets/teams/images.jpeg',
+      linkedin: ''
+    },
+    {
+      name: 'Prabhu Anpat',
+      position: 'Tech Member',
+      image: '/assets/teams/images.jpeg',
+      linkedin: ''
+    },
+    {
+      name: 'Anjali Rambhad',
+      position: 'Tech Member',
+      image: '/assets/teams/images.jpeg',
+      linkedin: ''
+    },
+    {
+      name: 'Neeraj Magdum',
+      position: 'Tech Member',
+      image: '/assets/teams/images.jpeg',
+      linkedin: ''
+    },
+    {
+      name: 'Pratik Patil',
+      position: 'Tech Member',
+      image: '/assets/teams/images.jpeg',
+      linkedin: ''
+    },
+    {
+      name: 'Sakshi Sangale',
+      position: 'Tech Member',
+      image: '/assets/teams/images.jpeg',
+      linkedin: ''
+    },
+    {
+      name: 'Aahana Padole',
+      position: 'Tech Member',
+      image: '/assets/teams/Aahana.jpg',
+      linkedin: 'https://www.linkedin.com/in/aahana-padole/'
+    },
+    {
+      name: 'Dev Taneja',
+      position: 'Tech Member',
+      image: '/assets/teams/images.jpeg',
+      linkedin: ''
+    },
+    {
+      name: 'Aditya Balghat',
+      position: 'Tech Member',
+      image: '/assets/teams/images.jpeg',
+      linkedin: ''
+    },
+    {
+      name: 'Kalpesh Sirsath',
+      position: 'Tech Member',
+      image: '/assets/teams/images.jpeg',
+      linkedin: ''
+    },
+];
+
+export const Leads = [
+    {
+      name: 'Tanish Khot',
+      position: 'Overall Lead',
+      image: '/assets/teams/images.jpeg',
+      linkedin: ''
+    },
+    {
+      name: 'Aashlesh Wawge',
+      position: 'Overall Lead',
+      image: '/assets/teams/images.jpeg',
+      linkedin: ''
+    },
+    {
+      name: 'Anjali Changedia',
+      position: 'Overall Lead',
+      image: '/assets/teams/images.jpeg',
+      linkedin: ''
+    },
+    {
+      name: 'Harsha Pareek',
+      position: 'Overall Lead',
+      image: '/assets/teams/images.jpeg',
+      linkedin: ''
+    },
+    {
+      name: 'Aditya Uttarwar',
+      position: 'Operational Lead',
+      image: '/assets/teams/images.jpeg',
+      linkedin: ''
+    },
+    {
+      name: 'Prabhu Anpat',
+      position: 'Operational Lead',
+      image: '/assets/teams/images.jpeg',
+      linkedin: ''
+    },
+    {
+      name: 'Madhura Pawar',
+      position: 'Operational Lead',
+      image: '/assets/teams/images.jpeg',
+      linkedin: ''
+    },
+    {
+      name: 'Atharva Raut',
+      position: 'Operational Lead',
+      image: '/assets/teams/images.jpeg',
+      linkedin: ''
+    },
+    {
+      name: 'Kartik Tichkule',
+      position: 'Marketing Lead',
+      image: '/assets/teams/image.jpeg',
+      linkedin: ''
+    },
+    {
+      name: 'Manasi Waghe',
+      position: 'Marketing Lead',
+      image: '/assets/teams/image.jpeg',
+      linkedin: ''
+    },
+    {
+      name: 'Anish Kachane',
+      position: 'Marketing Lead',
+      image: '/assets/teams/image.jpeg',
+      linkedin: ''
+    },
+    {
+      name: 'Sameer Dhande',
+      position: 'Marketing Lead',
+      image: '/assets/teams/image.jpeg',
+      linkedin: ''
+    },
+    {
+      name: 'Ayush Deshmukh',
+      position: 'Marketing Lead',
+      image: '/assets/teams/image.jpeg',
+      linkedin: ''
+    },
+    {
+      name: 'Vidhi Koul',
+      position: 'Marketing Lead',
+      image: '/assets/teams/image.jpeg',
+      linkedin: ''
+    },
+    {
+      name: 'Sarvesh Joshi',
+      position: 'Marketing Lead',
+      image: '/assets/teams/image.jpeg',
+      linkedin: ''
+    },
+    {
+      name: 'Neeraj Magdum',
+      position: 'Budget Lead',
+      image: '/assets/teams/image.jpeg',
+      linkedin: ''
+    },
+    {
+      name: 'Shreeya Daga',
+      position: 'Budget Lead',
+      image: '/assets/teams/image.jpeg',
+      linkedin: ''
+    },
+    {
+      name: 'Rishikesh Chaudhari',
+      position: 'Social Media Lead',
+      image: '/assets/teams/image.jpeg',
+      linkedin: ''
+    },
+    {
+      name: 'Shraddha Gangurde',
+      position: 'Social Media Lead',
+      image: '/assets/teams/image.jpeg',
+      linkedin: ''
+    },
+    {
+      name: 'Gaurav Waghmare',
+      position: 'Video Editing Lead',
+      image: '/assets/teams/image.jpeg',
+      linkedin: ''
+    },
+    {
+      name: 'Pratik Survase',
+      position: 'Publicity Lead',
+      image: '/assets/teams/image.jpeg',
+      linkedin: ''
+    },
+    {
+      name: 'Pranav Kale',
+      position: 'Publicity Lead',
+      image: '/assets/teams/image.jpeg',
+      linkedin: ''
+    },
+    {
+      name: 'Ishwari Gadewar',
+      position: 'Publicity Lead',
+      image: '/assets/teams/image.jpeg',
+      linkedin: ''
+    },
+    {
+      name: 'Rutuja Gangawane',
+      position: 'Publicity Lead',
+      image: '/assets/teams/image.jpeg',
+      linkedin: ''
+    },
+    {
+      name: 'Ajinkya Kachare',
+      position: 'Decoration Lead',
+      image: '/assets/teams/image.jpeg',
+      linkedin: ''
+    },
+    {
+      name: 'Prathamesh Naik',
+      position: 'Decoration Lead',
+      image: '/assets/teams/image.jpeg',
+      linkedin: ''
+    },
+    {
+      name: 'Vedika Thakur',
+      position: 'Decoration Lead',
+      image: '/assets/teams/image.jpeg',
+      linkedin: ''
+    },
+    {
+      name: 'Sonali Birajdar',
+      position: 'Decoration Lead',
+      image: '/assets/teams/image.jpeg',
+      linkedin: ''
+    },
+    {
+      name: 'Diya Bhat',
+      position: 'Design Lead',
+      image: '/assets/teams/image.jpeg',
+      linkedin: ''
+    },
+    {
+      name: 'Soham Phatak',
+      position: 'Design Lead',
+      image: '/assets/teams/image.jpeg',
+      linkedin: ''
+    },
+    {
+      name: 'Tanisha Jain',
+      position: 'Flashmob Lead',
+      image: '/assets/teams/image.jpeg',
+      linkedin: ''
+    },
+    {
+      name: 'Tanvi Pattewar',
+      position: 'Flashmob Lead',
+      image: '/assets/teams/image.jpeg',
+      linkedin: ''
+    },
+];

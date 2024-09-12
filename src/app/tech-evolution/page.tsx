e4966675ea00302ce9d6c0e51e78508dea29585a
Shreekar11 <shreekargade2004@gmail.com>
2024-09-13 01:08:39 +0530

    tech evolution pr commit

--- src/app/tech-evolution/page.tsx
+++ src/app/tech-evolution/page.tsx
@@ -1,10 +1,6 @@
 "use client"
 import React from 'react';
-import Image from 'next/image';
-// import TechCard from '@/components/TechEvo/TechCard';
-import {tech} from '../../data/techData'
 import TechCard from '@/components/TechEvo/TechCard';
-// import t from '../../../public/assets/Tech-Evolution/'
 
 const TechEvo = () => {
   return (

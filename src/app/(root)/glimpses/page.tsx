c2644853294198b7f6d25f9018b4f2df2f718317
AdityaMore7000 <adi.more7000@gmail.com>
2024-09-15 17:21:43 +0530

    add font and glimpses

---
+++ src/app/(root)/glimpses/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+import images from '../../../../public/assets/data/glimpses.json'
+import { useEffect } from "react";
+import Image from 'next/image';
+import localFont from "next/font/local";
+
+const originText = localFont({src:"../../../../public/fonts/OriginTech personal use.ttf"})
+export default function Page() {
+    useEffect(() => {
+        console.log('first')
+    }, [])
+    return (
+        <>
+        <div className="mt-5">
+            <h1 className={` ${originText.className} text-white text-5xl text-center mt-20`}>GLIMPSES</h1>
+        </div>
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-20 mx-auto mt-4 p-4 justify-items-center items-center'>
+            {images.map((image,index)=>{
+               return(
+                <Image key={index} src={image.thumbnail} className='rounded-lg' height={300} width={400} alt='image'/>
+               )
+            })}
+        </div>
+        </>
+    )
+}

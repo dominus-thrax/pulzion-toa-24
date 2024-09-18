f6bad5d3f589d9059850d90a3c36a40d24b58c16
gaurav662004 <gbwaghmare66@gmail.com>
2024-09-18 23:37:06 +0530

    hero section and about us updated

--- src/components/landing/about-us.tsx
+++ src/components/landing/about-us.tsx
@@ -6,6 +6,7 @@ import localFont from "next/font/local";
 import card from "../../../public/assets/landing/about-us/card.png";
 import NumberTicker from "../magicui/number-ticker";
 import Meteors from "../magicui/meteors";
+import { CardSpotlight } from "../ui/card-spotlight";
 
 const sixtyfour = localFont({
   src: "../../../public/font/OriginTech personal use.ttf",
@@ -16,7 +17,6 @@ const font = localFont({
 });
 
 const About: React.FC = () => {
-  
   const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
@@ -37,98 +37,42 @@ const About: React.FC = () => {
   }, []);
 
   return (
-    <div className="py-8 md:py-16 relative min-h-screen w-full flex flex-col justify-center items-center bg-black overflow-hidden">
+    <div className="pb-8 md:py-16 relative min-h-screen w-full flex flex-col justify-center items-center bg-black overflow-hidden">
       {/* Heading */}
       <h1
-        className={`${sixtyfour.className} text-2xl md:text-5xl lg:text-6xl text-[#cfc36d] text-center mb-8 pt-32 glowing-text`}
+        className={`${sixtyfour.className} text-4xl md:text-5xl lg:text-6xl text-[#cfc36d] text-center mb-8 pt-32 glowing-text`}
       >
         ABOUT US
       </h1>
 
       {/* Content Section */}
       <div className="mx-auto flex flex-col lg:flex-row items-center lg:items-start w-full relative z-10">
-        {/* Left Section: Circles */}
-        <div
-          id="animated-circles"
-          className={`${
-            font.className
-          } relative w-full lg:w-1/2 flex justify-center  transition-all duration-1000 ease-in-out transform ${
-            isVisible
-              ? "translate-x-0 opacity-100"
-              : "-translate-x-full opacity-0"
-          }`}
-        >
-          <div className="grid grid-cols-2 gap-3 md:gap-5 place-items-center justify-center items-center lg:mt-5 ">
-            {/* Top Left Circle */}
-            <div className="relative w-[150px] h-[150px] md:w-[205px] md:h-[205px] flex items-center justify-center">
-              <div className="absolute inset-0 flex items-center justify-center text-white text-center">
-                <div className="absolute inset-0 flex items-center justify-center">
-                  <div className="absolute w-full h-full rounded-full border-r-[4px] border-primary animate-spin-slow z-20 border-x-emerald-400"></div>
-                </div>
-
-                <div className="absolute inset-0 flex items-center justify-center">
-                  <div className="absolute w-[90%] h-[90%] rounded-full border-l-[4px] border-secondary animate-reverse-spin z-0 border-yellow-300"></div>
-                </div>
-                <span className="text-xs md:text-sm font-semibold">
-                  <p className="whitespace-pre-wrap text-3xl font-medium tracking-tighter text-white dark:text-black">
-                    <NumberTicker value={100} />
-                  </p>
-                  <h2 className="text-xl">Events</h2>
-                </span>
-              </div>
-            </div>
-
-            {/* Top Right Circle */}
-            <div className="relative w-[150px] h-[150px] md:w-[205px] md:h-[205px] flex items-center justify-center">
-              <div className="absolute inset-0 flex items-center justify-center text-white text-center">
-                <div className="absolute inset-0 flex items-center justify-center">
-                  <div className="absolute w-full h-full rounded-full border-r-[4px] border-primary animate-spin-slow z-20 border-x-emerald-400"></div>
-                </div>
-
-                <div className="absolute inset-0 flex items-center justify-center">
-                  <div className="absolute w-[90%] h-[90%] rounded-full border-l-[4px] border-secondary animate-reverse-spin z-0 border-yellow-300"></div>
-                </div>
-                <span className="text-xs md:text-sm font-semibold">
-                  <p className="whitespace-pre-wrap text-3xl font-medium tracking-tighter text-white dark:text-black">
-                    <NumberTicker value={300} />
-                  </p>
-                  <h2>VOLUNTEERS</h2>
-                </span>
-              </div>
-            </div>
-
-            {/* Bottom Center Circle */}
-            <div className="relative col-span-2 w-[150px] h-[150px] md:w-[205px] md:h-[205px] flex items-center justify-center">
-              <div className="absolute inset-0 flex items-center justify-center text-white text-center">
-                <div className="absolute inset-0 flex items-center justify-center">
-                  <div className="absolute w-full h-full rounded-full border-r-[4px] border-primary animate-spin-slow z-20 border-x-emerald-400"></div>
-                </div>
-
-                <div className="absolute inset-0 flex items-center justify-center">
-                  <div className="absolute w-[90%] h-[90%] rounded-full border-l-[4px] border-secondary animate-reverse-spin z-0 border-yellow-300"></div>
-                </div>
-                <span className="text-xs md:text-sm font-semibold">
-                  <p className="whitespace-pre-wrap text-3xl font-medium tracking-tighter text-white dark:text-black">
-                    <NumberTicker value={8000} />
-                  </p>
-                  <h2>FOOTFALL</h2>
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
-
         {/* Right Section: Text */}
         <div
-          className={`${font.className} p-8 md:p-2 w-full lg:w-1/2 text-center text-[#88B788] text-sm md:text-xl lg:text-2xl max-w-lg space-y-6 mt-8 lg:mt-0 flex items-center justify-center`}
+          className={`${font.className} p-4 md:p-2 w-full text-center text-[#88B788] text-sm md:text-xl lg:text-2xl  space-y-6 mt-8 lg:mt-0 flex items-center justify-center md:px-14 lg:px-44 `}
         >
-          <div className="flex flex-col items-center space-y-6">
+          <div className="flex flex-col items-center space-y-6 ">
             <div
-              className={`${font.className} md:p-2 w-full lg:w-full text-center text-[#88B788] text-sm md:text-xl lg:text-2xl max-w-lg space-y-6 mt-8 lg:mt-0 flex items-center justify-center`}
+              className={`${font.className} md:p-6 w-full text-center text-[#88B788] text-sm md:text-xl lg:text-2xl  space-y-6 mt-8 lg:mt-0 flex items-center justify-center`}
               style={{ height: "100%" }}
             >
-              <div className="flex flex-col items-center lg:p-5 space-y-6">
-                <p className="text-lg text-justify">
+              <div className="flex flex-col items-center lg:p-5 p-2 space-y-6 ring-1 ring-[#ADCC8B] rounded-2xl">
+                <div className="w-full flex flex-col md:flex-row lg:flex-row mx-auto gap-10 items-center justify-center p-5">
+                  <div className="w-44 text-5xl ring-1 ring-[#ADCC8B] rounded-2xl p-2 mb-4">
+                    <NumberTicker value={100} className="text-[#CFC36D]" />
+                    <p className="text-2xl">Events</p>
+                  </div>
+                  <div className="w-44 text-5xl ring-1 ring-[#ADCC8B] rounded-2xl p-2 mb-4">
+                    <NumberTicker value={300} className="text-[#CFC36D]" />
+                    <p className="text-2xl">Volunteers</p>
+                  </div>
+                  <div className="w-44 text-5xl ring-1 ring-[#ADCC8B] rounded-2xl p-2 mb-4">
+                    <NumberTicker value={8000} className="text-[#CFC36D]" />
+                    <p className="text-2xl">Events</p>
+                  </div>
+                </div>
+
+                <p className="text-lg text-justify px-2">
                   Pulzion is the annual flagship event organized by PICT ACM
                   Student Chapter (PASC). Pulzion consists of multiple events in
                   technical as well as non-technical domains includes coding
@@ -151,11 +95,11 @@ const About: React.FC = () => {
       </div>
 
       {/* Bottom Cards */}
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mt-8 z-30">
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 mt-8 z-20 p-4 lg:w-full lg:justify-center lg:mx-auto md:-mb-64">
         {/* Card 1 */}
-        <div className="relative max-w-sm w-full ">
+        <div className="relative max-w-sm w-full mx-auto">
           <div className="relative">
-            <div className="relative border-[#ADCC8B] rounded-xl flex h-[200px] w-full flex-col items-center justify-center bg-black overflow-hidden border bg-background md:shadow-xl">
+            <div className="relative border-[#ADCC8B] rounded-xl flex h-56 w-full flex-col items-center justify-center bg-black overflow-hidden border bg-background md:shadow-xl">
               <Meteors number={30} />
               <div
                 className={`${font.className} absolute inset-0 flex flex-col justify-center items-center z-10 text-white p-6`}
@@ -173,9 +117,9 @@ const About: React.FC = () => {
         </div>
 
         {/* Card 2 */}
-        <div className="relative max-w-sm w-full">
+        <div className="relative max-w-sm w-full mx-auto">
           <div className="relative">
-            <div className="relative border-[#ADCC8B] rounded-xl flex h-[200px] w-full flex-col items-center justify-center bg-black overflow-hidden border bg-background md:shadow-xl">
+            <div className="relative border-[#ADCC8B] rounded-xl flex h-56 w-full flex-col items-center justify-center bg-black overflow-hidden border bg-background md:shadow-xl">
               <Meteors number={30} />
               <div
                 className={`${font.className} absolute inset-0 flex flex-col justify-center items-center z-10 text-white p-6`}
@@ -194,9 +138,9 @@ const About: React.FC = () => {
         </div>
 
         {/* Card 3 */}
-        <div className="relative max-w-sm w-full ">
+        <div className="relative max-w-sm w-full mx-auto">
           <div className="relative">
-            <div className="relative border-[#ADCC8B] rounded-xl flex h-[200px] w-full flex-col items-center justify-center bg-black overflow-hidden border bg-background md:shadow-xl">
+            <div className="relative border-[#ADCC8B] rounded-xl flex h-56 w-full flex-col items-center justify-center bg-black overflow-hidden border bg-background md:shadow-xl">
               <Meteors number={30} />
               <div
                 className={`${font.className} absolute inset-0 flex flex-col justify-center items-center z-10 text-white p-6`}

f88d6262ebf0f901df5a4ac5e31c2ee9a46b86d0
gaurav662004 <gbwaghmare66@gmail.com>
2024-09-22 00:33:27 +0530

    bg changed

--- src/components/landing/hero.tsx
+++ src/components/landing/hero.tsx
@@ -6,6 +6,7 @@ import logo from "../../../public/assets/landing/hero/logo.png";
 import localFont from "next/font/local";
 import MatrixRain from "./MatrixRain";
 import { useAuth } from "@/context";
+import { AuroraBackground } from "../ui/aurora-background";
 
 const sixtyfour = localFont({
   src: "../../../public/font/OriginTech personal use.ttf",
@@ -56,56 +57,57 @@ const Hero = () => {
           </video>
         </>
       )}
+      <AuroraBackground>
+        {/* Main Content Wrapper */}
+        <div
+          className={`relative flex flex-col pt-[3rem] md:pt-[5rem] w-full justify-center transition-transform duration-500 ease-in-out ${
+            contentVisible ? "scale-100 opacity-100" : "scale-50 opacity-0"
+          }`}
+        >
+          {/* <div className="w-full"> */}
 
-      {/* Main Content Wrapper */}
-      <div
-        className={`relative flex flex-col pt-[3rem] md:pt-[9rem] w-full justify-center transition-transform duration-500 ease-in-out ${
-          contentVisible ? "scale-100 opacity-100" : "scale-50 opacity-0"
-        }`}
-      >
-        {/* <div className="w-full"> */}
-        <MatrixRain></MatrixRain>
-        {/* </div> */}
+          {/* </div> */}
 
-        <div className="flex flex-col justify-center items-center relative z-10 space-y-[5rem] p-10 md:p-0">
-          {/* Logo and Tagline */}
-          <div className="flex flex-col justify-center items-center w-full">
-            <Image
-              src={logo}
-              alt="Main Logo"
-              width={700}
-              height={300}
-              quality={100}
-              layout="fixed"
-              className="mt-28"
-            />
-          </div>
-          <div
-            className={`${sixtyfour.className} pb-10 md:pb-[3rem] text-[#CFC36D] space-y-4 text-center text-xl md:text-3xl`}
-          >
-            <div>The annual techfest of PICT ACM Student Chapter</div>
-            <div className="">3rd, 4th & 5th October</div>
+          <div className="flex flex-col justify-center items-center relative z-10 space-y-[5rem] p-10 md:p-0">
+            {/* Logo and Tagline */}
+            <div className="flex flex-col justify-center items-center w-full">
+              <Image
+                src={logo}
+                alt="Main Logo"
+                width={700}
+                height={300}
+                quality={100}
+                layout="fixed"
+                className="mt-0"
+              />
+            </div>
+            <div
+              className={`${sixtyfour.className} pb-10 md:pb-[3rem] text-[#CFC36D] space-y-4 text-center text-xl md:text-3xl`}
+            >
+              <div>The annual techfest of PICT ACM Student Chapter</div>
+              <div className="">3rd, 4th & 5th October</div>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="w-full flex justify-center">
-        {user.token ? (
-          <a
-            href="/events"
-            className={`${font.className} z-10 hover:cursor-pointer text-black bg-[#ECAA43] px-4 py-2 rounded-2xl mx-auto block text-center absolute`}
-          >
-            Explore Events
-          </a>
-        ) : (
-          <a
-            href="/register"
-            className={`${font.className} z-10 hover:cursor-pointer text-black bg-[#ECAA43] px-4 py-2 rounded-2xl mx-auto block text-center absolute`}
-          >
-            Participate Now
-          </a>
-        )}
-      </div>
+        <div className="w-full flex justify-center">
+          {user.token ? (
+            <a
+              href="/events"
+              className={`${font.className} z-10 hover:cursor-pointer text-black bg-[#ECAA43] px-4 py-2 rounded-2xl mx-auto block text-center absolute`}
+            >
+              Explore Events
+            </a>
+          ) : (
+            <a
+              href="/register"
+              className={`${font.className} z-10 hover:cursor-pointer text-black bg-[#ECAA43] px-4 py-2 rounded-2xl mx-auto block text-center absolute`}
+            >
+              Participate Now
+            </a>
+          )}
+        </div>
+      </AuroraBackground>
     </div>
   );
 };
